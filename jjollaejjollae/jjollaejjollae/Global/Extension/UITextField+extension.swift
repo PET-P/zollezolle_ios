b83739c464f921b11c8888a1b40f730ebfd891e1
DOHYUN <pastapeter@naver.com>
2021-08-05 01:00:44 +0900

    refactor: UITextField 아랫줄 height 2로 올림

--- jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
@@ -5,17 +5,23 @@
 //  Created by abc on 2021/08/04.
 //
 
-import Foundation
+import UIKit
 
 extension UITextField {
     
     func underlineStyle(textColor: UIColor, borderColor: UIColor) {
         self.borderStyle = .none
-        let border = CALayer()
-        border.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.size.width, height: 1)
-        border.backgroundColor = borderColor.cgColor
-        self.layer.addSublayer(border)
-        self.textAlignment = .right
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.size.width, height: 2)
+        bottomLine.backgroundColor = borderColor.cgColor
+        self.layer.addSublayer(bottomLine)
+//        self.textAlignment = .right
         self.textColor = textColor
+    }
+    
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
     }
 }
