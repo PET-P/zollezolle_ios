a0fc039a8bf1729ae6df3de40776fb680c69396f
DOHYUN <pastapeter@naver.com>
2021-08-04 17:15:11 +0900

    feat: LoginViewcontroller, storyboard 추가

---
+++ jjollaejjollae/jjollaejjollae/Screen/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/04.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var JLTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
