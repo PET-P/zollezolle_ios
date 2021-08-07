b02b4c95ededa773abc3c80d7a973e3a0795392d
pastapeter <pastapeter@naver.com>
2021-08-07 23:07:21 +0900

    feat: UITextField익스텐션에 calayer 변경함수 추가

--- jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
@@ -11,14 +11,10 @@ extension UITextField {
     
     func underlineStyle(textColor: UIColor, borderColor: UIColor, width: CGFloat) {
         self.borderStyle = .none
-        print("newwidth \(width)")
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0, y: self.frame.size.height-10, width: width - 64 , height: 2)
         bottomLine.backgroundColor = borderColor.cgColor
-        
-        print(self.frame.size.width)
         self.layer.addSublayer(bottomLine)
-//        self.textAlignment = .right
         self.textColor = textColor
     }
     
@@ -27,20 +23,16 @@ extension UITextField {
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
-    
-    func underline(){
-        self.borderStyle = .none
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: bounds.size.height+3))
-        path.addLine(to: CGPoint(x: bounds.width, y: bounds.size.height+3))
-        path.close()
         
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
-        shapeLayer.lineWidth = path.lineWidth + 1
-        shapeLayer.fillColor = UIColor.쫄래페일그린.cgColor
-        shapeLayer.strokeColor = UIColor.쫄래페일그린.cgColor
-        layer.addSublayer(shapeLayer)
+    func changeUnderLine(borderColor: UIColor, width: CGFloat) {
+        self.layer.sublayers?[0].removeFromSuperlayer()
+        self.borderStyle = .none
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height+5, width: width - 64 , height: 2)
+        bottomLine.backgroundColor = borderColor.cgColor
+        self.textColor = UIColor.회
+        self.layer.insertSublayer(bottomLine, at: 0)
+        self.setNeedsDisplay()
     }
 
 }
---
+++ jjollaejjollae/jjollaejjollae/Screen/Login/ViewController/LoginManager.swift
@@ -0,0 +1,8 @@
+//
+//  LoginManager.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/07.
+//
+
+import Foundation
