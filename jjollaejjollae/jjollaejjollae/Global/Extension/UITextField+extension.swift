6523aee4898aad9495b99ecc84c29b0cf4b4cf21
pastapeter <pastapeter@naver.com>
2021-08-07 18:44:18 +0900

    fix: Textfield 아래 밑줄, 모든 기기에 맞출 수 있도록 함

--- jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UITextField+extension.swift
@@ -9,11 +9,14 @@ import UIKit
 
 extension UITextField {
     
-    func underlineStyle(textColor: UIColor, borderColor: UIColor) {
+    func underlineStyle(textColor: UIColor, borderColor: UIColor, width: CGFloat) {
         self.borderStyle = .none
+        print("newwidth \(width)")
         let bottomLine = CALayer()
-        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.size.width, height: 2)
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height-10, width: width - 64 , height: 2)
         bottomLine.backgroundColor = borderColor.cgColor
+        
+        print(self.frame.size.width)
         self.layer.addSublayer(bottomLine)
 //        self.textAlignment = .right
         self.textColor = textColor
@@ -24,4 +27,21 @@ extension UITextField {
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
+    
+    func underline(){
+        self.borderStyle = .none
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: bounds.size.height+3))
+        path.addLine(to: CGPoint(x: bounds.width, y: bounds.size.height+3))
+        path.close()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = path.lineWidth + 1
+        shapeLayer.fillColor = UIColor.쫄래페일그린.cgColor
+        shapeLayer.strokeColor = UIColor.쫄래페일그린.cgColor
+        layer.addSublayer(shapeLayer)
+    }
+
 }
+
