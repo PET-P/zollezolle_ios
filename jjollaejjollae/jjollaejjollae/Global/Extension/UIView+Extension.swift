9fe1fd57cea90048e4e5d1503d04954cd0cef372
DOHYUN CHUNG <pastapeter@naver.com>
2021-08-15 17:47:58 +0900

    Feat: border 만드는 함수 추가

--- jjollaejjollae/jjollaejjollae/Global/Extension/UIView+Extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UIView+Extension.swift
@@ -8,36 +8,56 @@
 import UIKit
 
 extension UIView {
-    
-    func setRounded(radius : CGFloat?) {
-        // UIView 모서리 둥글때
-        if let cornerRadius_ = radius {
-            self.layer.cornerRadius = cornerRadius_
-        } else {
-            // if nil -> go default
-            self.layer.cornerRadius = self.layer.frame.height / 2
-        }
-        
-        self.layer.masksToBounds = true
+  
+  func setRounded(radius : CGFloat?) {
+    // UIView 모서리 둥글때
+    if let cornerRadius_ = radius {
+      self.layer.cornerRadius = cornerRadius_
+    } else {
+      // if nil -> go default
+      self.layer.cornerRadius = self.layer.frame.height / 2
     }
     
-    func setBorder(borderColor : UIColor?, borderWidth : CGFloat?) {
-        // UIView 테두리 설정
-        
-        // 색상 설정 default값은 쫄래페일그린
-        if let borderColor_ = borderColor {
-            self.layer.borderColor = borderColor_.cgColor
-        } else {
-            self.layer.borderColor = UIColor.쫄래페일그린.cgColor
-        }
-        
-        // UIView의 테두리 두께 설정
-        if let borderWidth_ = borderWidth {
-            self.layer.borderWidth = borderWidth_
-        } else {
-            // borderWidth 변수가 nil일 경우의 default 1포인트
-            self.layer.borderWidth = 1.0
-        }
+    self.layer.masksToBounds = true
+  }
+  
+  func setBorder(borderColor : UIColor?, borderWidth : CGFloat?) {
+    // UIView 테두리 설정
+    
+    // 색상 설정 default값은 쫄래페일그린
+    if let borderColor_ = borderColor {
+      self.layer.borderColor = borderColor_.cgColor
+    } else {
+      self.layer.borderColor = UIColor.쫄래페일그린.cgColor
     }
+    
+    // UIView의 테두리 두께 설정
+    if let borderWidth_ = borderWidth {
+      self.layer.borderWidth = borderWidth_
+    } else {
+      // borderWidth 변수가 nil일 경우의 default 1포인트
+      self.layer.borderWidth = 1.0
+    }
+  }
+  func addBorder(_ arrEdge: [UIRectEdge], color: UIColor, width: CGFloat){
+    for edge in arrEdge {
+      let border = CALayer()
+      switch edge {
+      case UIRectEdge.top:
+        border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+      case UIRectEdge.bottom:
+        border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+      case UIRectEdge.left:
+        border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+      case UIRectEdge.right:
+        border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+      default:
+        break
+      }
+      border.backgroundColor = color.cgColor
+      self.layer.addSublayer(border)
+    }
+  }
+  
 }
 
