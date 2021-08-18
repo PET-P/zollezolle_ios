9b242dc6e7feca515e4bf467d5ae861ff39f46f4
DOHYUN CHUNG <pastapeter@naver.com>
2021-08-19 02:42:33 +0900

    feat: 검색결과 화면 중, 검색결과가 없을 때의 화면 UI
    아래 간단한 model 만들어놨는데, 나중에 model확정시 변경 필요

---
+++ jjollaejjollae/jjollaejjollae/Screen/Search/View/SearchResultDataSource.swift
@@ -0,0 +1,46 @@
+//
+//  SearchResultDataSource.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/19.
+//
+
+import UIKit
+
+class SearchResultDataSource: NSObject, UITableViewDataSource {
+  var dataList : [SearchResultInfo] = []
+  lazy var likes: [Int: Int] = [:]
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //    return list.count
+    return 10
+  }
+}
+
+extension SearchResultDataSource: SearchResultCellDelegate {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath)
+            as? SearchResultTableViewCell else { return UITableViewCell()}
+    
+    cell.delegate = self
+    cell.index = indexPath.row
+    let item = dataList[indexPath.row]
+    cell.locationNameLabel.text = item.name
+    cell.locationTypeLabel.text = item.type
+    cell.numberOfReviewsLabel.text = "(\(item.numbers))"
+    cell.starPointLabel.text = "\(item.points)"
+    
+    cell.isWish = likes[indexPath.row] == 1 ? true : false
+    dataList[indexPath.row].like = likes[indexPath.row] == 1 ? true : false
+    
+    return cell
+  }
+  
+  func didTapHeart(for index: Int, like: Bool) {
+    if like {
+      likes[index] = 1
+    } else {
+      likes[index] = 0
+    }
+  }
+}
---
+++ jjollaejjollae/jjollaejjollae/Screen/Search/View/SearchResultTableViewCell.swift
@@ -0,0 +1,115 @@
+//
+//  SearchResultTableViewCell.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/16.
+//
+
+import UIKit
+
+protocol SearchResultCellDelegate {
+  func didTapHeart(for index: Int, like: Bool)
+}
+
+class SearchResultTableViewCell: UITableViewCell {
+  //MARK: - IBOUTLET
+  @IBOutlet weak var cellImageView: UIImageView! {
+    didSet {
+      cellImageView.setRounded(radius: 8)
+      cellImageView.isUserInteractionEnabled = true
+    }
+  }
+  
+  @IBOutlet weak var addressLabel: UILabel! {
+    didSet {
+      addressLabel.isHidden = true
+      addressLabel.font = .robotoMedium(size: 12)
+      addressLabel.textColor = .회
+    }
+  }
+  
+  @IBOutlet weak var locationNameLabel: UILabel! {
+    didSet {
+      locationNameLabel.font = .robotoBold(size: 16)
+      locationNameLabel.textColor = .쥐색38
+    }
+  }
+  @IBOutlet weak var locationTypeLabel: UILabel! {
+    didSet {
+      locationTypeLabel.font = .robotoMedium(size: 12)
+      locationTypeLabel.textColor = .회
+    }
+  }
+  @IBOutlet weak var starPointLabel: UILabel! {
+    didSet {
+      starPointLabel.font = .robotoBold(size: 12)
+      starPointLabel.textColor = .회
+    }
+  }
+  @IBOutlet weak var numberOfReviewsLabel: UILabel! {
+    didSet {
+      numberOfReviewsLabel.font = .robotoRegular(size: 10)
+      numberOfReviewsLabel.textColor = .회
+    }
+  }
+  @IBOutlet weak var likeButton: UIButton!
+  @IBOutlet weak var priceLabel: UILabel! {
+    didSet {
+      priceLabel.isHidden = true
+      priceLabel.textColor = .쥐색38
+      priceLabel.font = .robotoBold(size: 16)
+    }
+  }
+  
+  @IBOutlet weak var DaysLabel: UILabel! {
+    didSet {
+      DaysLabel.isHidden = true
+      DaysLabel.textColor = .회
+      DaysLabel.font = .robotoMedium(size: 10)
+    }
+  }
+  
+  var isWish: Bool? {
+    didSet {
+      if isWish == true {
+        likeButton.setImage(UIImage(named: "pinkLike"), for: .normal)
+      } else {
+        likeButton.setImage(UIImage(named: "grayLike"), for: .normal)
+      }
+    }
+  }
+  
+  var delegate : SearchResultCellDelegate?
+  var index : Int?
+  
+  @IBAction func didTapHeart(_ sender: UIButton) {
+    guard let index = index else {return}
+    if sender.isSelected {
+      isWish = true
+      delegate?.didTapHeart(for: index, like: true)
+    } else {
+      isWish = false
+      delegate?.didTapHeart(for: index, like: false)
+    }
+    sender.isSelected = !sender.isSelected
+  }
+
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    let padding = UIEdgeInsets(top: 0, left: 0, bottom: 42, right: 0)
+    contentView.frame = contentView.frame.inset(by: padding)
+  }
+  
+  
+}
+
+
