ef79b39087147b84720c9e7192a3d331f82aad33
DOHYUN CHUNG <pastapeter@naver.com>
2021-08-20 05:17:52 +0900

    feat: 각 버튼에 대한 데이터 소스 변환과, 드롭다운 메뉴 구현

---
+++ jjollaejjollae/jjollaejjollae/Screen/Search/View/DropDownDataSource.swift
@@ -0,0 +1,27 @@
+//
+//  DropDownDataSource.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/20.
+//
+
+import UIKit
+
+class DropDownDataSource: NSObject, UITableViewDataSource {
+  var dataList = ["리뷰 많은순", "최근 등록순", "별점 높은순"]
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dataList.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = dataList[indexPath.row]
+    cell.textLabel?.font = .robotoMedium(size: 13)
+    cell.textLabel?.textColor = .회
+    cell.textLabel?.setContentHuggingPriority(.required, for: .horizontal)
+    cell.textLabel?.textAlignment = .center
+    cell.separatorInset = UIEdgeInsets.zero
+    return cell
+  }
+}
--- jjollaejjollae/jjollaejjollae/Screen/Search/ViewController/SearchResultViewController.swift
+++ jjollaejjollae/jjollaejjollae/Screen/Search/ViewController/SearchResultViewController.swift
@@ -2,99 +2,247 @@
 //  SearchResultViewController.swift
 //  jjollaejjollae
 //
-//  Created by abc on 2021/08/16.
+//  Created by abc on 2021/08/19.
 //
 
 import UIKit
-import XLPagerTabStrip
 
-struct SearchResultInfo {
-  var name = "아지멍카페"
-  var type = "커피/브런치"
-  var points = 4.9
-  var numbers = 52
-  var like = false
+class CellClass: UITableViewCell {
+  
 }
 
-class SearchResultViewController: UIViewController, IndicatorInfoProvider {
-  
-  func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
-    return IndicatorInfo(title: "검색 결과")
-  }
-  
-  var dataList = [SearchResultInfo]()
-  lazy var likes : [Int: Int] = [:]
-  
-  //MARK: - OUTLET
-  @IBOutlet weak var SearchResultTableView: UITableView!
-  @IBOutlet weak var noInfoLabel: UILabel! {
+//MARK: - 임시로 모델만들기
+class ModelController: NSObject {
+  var accommoList = [SearchResultInfo(name: "여기는 숙소"), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(),]
+  var cafeList = [SearchResultInfo(name: "여기는 카페"), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(),]
+  var landmarkList = [SearchResultInfo(name: "여기는 관광지"), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(),]
+  var restaurantList = [SearchResultInfo(name: "여기는 맛집"), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(), SearchResultInfo(),]
+}
+
+class SearchResultViewController: UIViewController {
+  //MARK: - IBOUTLET
+  @IBOutlet weak var headView: UIView! {
     didSet {
-      noInfoLabel.font = .robotoRegular(size: 16)
-      noInfoLabel.textColor = .회
-      noInfoLabel.text = "\"멍멍이카페에 해당하는 정보가 없어요\""
+      headView.backgroundColor = .쫄래페일그린
     }
   }
-  @IBOutlet weak var searchGuide: UILabel! {
+  @IBOutlet weak var backButton: UIButton! {
     didSet {
-      searchGuide.font = .robotoRegular(size: 14)
-      searchGuide.textColor = .회
-      searchGuide.textAlignment = .center
-      searchGuide.numberOfLines = 2
-      searchGuide.text = "띄어쓰기 후 재검색 해 보세요! \n 예)멍멍이카페 → 멍멍이 카페"
+      backButton.tintColor = .쫄래블랙
     }
   }
   
-  private var noInfoLabelText: String = ""{
+  @IBOutlet weak var searchTextField: UITextField! {
     didSet {
-      noInfoLabel.text = "\(noInfoLabelText)에 해당하는 정보가 없어요"
+      searchTextField.setRounded(radius: nil)
+    }
+  }
+  @IBOutlet weak var restaurantButton: UIButton!
+  @IBOutlet weak var landMarkButton: UIButton!
+  @IBOutlet weak var cafeButton: UIButton!
+  @IBOutlet weak var accommodationButton: UIButton!
+  @IBOutlet weak var setScheduleButton: UIButton! {
+    didSet {
+      setScheduleButton.setRounded(radius: 4)
+      setScheduleButton.backgroundColor = UIColor(rgb: 0xF4F4F4)
+      setScheduleButton.setTitle("8월 10일 (화) - 8월 14일 (토)", for: .normal)
+      setScheduleButton.tintColor = .색44444
+      setScheduleButton.titleLabel?.font = .robotoRegular(size: 12)
+      setScheduleButton.contentEdgeInsets = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+    }
+  }
+  @IBOutlet weak var setPersonnelButton: UIButton! {
+    didSet {
+      setPersonnelButton.setRounded(radius: 4)
+      setPersonnelButton.backgroundColor = UIColor(rgb: 0xF4F4F4)
+      setPersonnelButton.setTitle("성인 2/강아지 1", for: .normal)
+      setPersonnelButton.tintColor = .색44444
+      setPersonnelButton.titleLabel?.font = .robotoRegular(size: 12)
+      setPersonnelButton.contentEdgeInsets = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 10)
+    }
+  }
+  @IBOutlet weak var setFilterButton: UIButton! {
+    didSet {
+      setFilterButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+      setFilterButton.tintColor = .색44444
+    }
+  }
+  @IBOutlet weak var scheduleStackView: UIStackView!
+  @IBOutlet weak var HeightTobeDynamioc: NSLayoutConstraint!
+  @IBOutlet weak var numberOfResultLabel: UILabel! {
+    didSet {
+      numberOfResultLabel.text = "0개 이상의 숙소"
+      numberOfResultLabel.font = .robotoRegular(size: 12)
+      numberOfResultLabel.textColor = .색44444
+    }
+  }
+  @IBOutlet var setOrderButton: UIButton! {
+    didSet {
+      setOrderButton.layer.borderWidth = 0
+      setOrderButton.setTitle("추천순 ▼", for: .normal)
+      setOrderButton.tintColor = .색44444
+      setOrderButton.titleLabel?.font = .robotoMedium(size: 13)
+    }
+  }
+  @IBOutlet var resultTableView: UITableView!
+  
+  @IBOutlet weak var separateLine: UIView! {
+    didSet {
+      separateLine.backgroundColor = .색e8
     }
   }
   
-  private var searchGuideText: String = "" {
-    didSet {
-      searchGuide.text = "띄어쓰기 후 재검색 해 보세요! \n 예)멍멍이카페 → 멍멍이 카페"
-    }
-  }
   
   let nib = UINib(nibName: "SearchResultTableViewCell", bundle: nil)
-  let searchResultDataSource = SearchResultDataSource()
+  var searchResultDataSources: [UITableViewDataSource] = []
+  var dataList = [SearchResultInfo]()
+  lazy var likes: [Int : Int] = [:]
+  
+  let accommodationDataSource = AccommodationDataSource()
+  let restaurantDataSource = RestaurantDataSource()
+  let cafeDataSource = CafeDataSource()
+  let landmarkDataSource = LandmarkDataSource()
+  let dropdownDataSource = DropDownDataSource()
+  
+  let modelController = ModelController()
+  var defaultHeight: CGFloat  = 0
+  var buttons : [UIButton] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    SearchResultTableView.dataSource = searchResultDataSource
-    SearchResultTableView.delegate = self
-    SearchResultTableView.register(nib, forCellReuseIdentifier: "resultCell")
-    SearchResultTableView.rowHeight = UITableView.automaticDimension
-    for _ in 0..<10 {
-      dataList.append(SearchResultInfo())
+    resultTableView.delegate = self
+    dropDownTableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
+    resultTableView.register(nib, forCellReuseIdentifier: "resultCell")
+    accommodationDataSource.dataList = modelController.accommoList
+    restaurantDataSource.dataList = modelController.restaurantList
+    cafeDataSource.dataList = modelController.cafeList
+    landmarkDataSource.dataList = modelController.landmarkList
+    searchResultDataSources = [accommodationDataSource, restaurantDataSource, cafeDataSource, landmarkDataSource]
+    resultTableView.dataSource = searchResultDataSources[0]
+    resultTableView.tableFooterView = UIView(frame: CGRect.zero)
+    dropDownTableView.dataSource = dropdownDataSource
+//    dropDownTableView.contentInset = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+    dropDownTableView.separatorStyle = .none
+    dropDownTableView.delegate = self
+    setLocationFilterButtonUI()
+    defaultHeight = HeightTobeDynamioc.constant
+  }
+  
+  private func setLocationFilterButtonUI() {
+    buttons = [restaurantButton, landMarkButton, cafeButton, accommodationButton]
+    buttons.forEach { (button) in
+      button.setRounded(radius: nil)
+      button.layer.borderColor = UIColor.쫄래페일그린.cgColor
+      button.layer.borderWidth = 1.0
+      button.backgroundColor = .white
+      button.tintColor = .쫄래블랙
+      button.titleLabel?.font = .robotoMedium(size: 13)
     }
-    searchResultDataSource.dataList = dataList
+  }
+  
+  private func updateButtonUI(_ sender: UIButton) {
+    buttons.forEach { button in
+      if button == sender {
+        button.backgroundColor = .쫄래페일그린
+      } else {
+        button.backgroundColor = .white
+      }
+    }
+  }
+  
+  @IBAction private func didTapRestaurantButton(_ sender: UIButton) {
+    resultTableView.dataSource = searchResultDataSources[1]
+    scheduleStackView.isHidden = true
+    HeightTobeDynamioc.constant = 0 - setFilterButton.frame.height
+    updateButtonUI(sender)
+    resultTableView.reloadData()
+  }
+  @IBAction private func didTapLandmarkButton(_ sender: UIButton) {
+    resultTableView.dataSource = searchResultDataSources[3]
+    scheduleStackView.isHidden = true
+    HeightTobeDynamioc.constant = 0 - setFilterButton.frame.height
+    updateButtonUI(sender)
+    resultTableView.reloadData()
+  }
+  @IBAction private func didTapCafeButton(_ sender: UIButton) {
+    resultTableView.dataSource = searchResultDataSources[2]
+    scheduleStackView.isHidden = true
+    print(setFilterButton.frame.height)
+    print(HeightTobeDynamioc.constant)
+    HeightTobeDynamioc.constant = 0 - setFilterButton.frame.height
+    updateButtonUI(sender)
+    resultTableView.reloadData()
+  }
+  @IBAction private func didTapAccommodationButton(_ sender: UIButton) {
+    resultTableView.dataSource = searchResultDataSources[0]
+    scheduleStackView.isHidden = false
+    HeightTobeDynamioc.constant = defaultHeight
+    updateButtonUI(sender)
+    resultTableView.reloadData()
+  }
+  
+  let transparentView = UIView()
+  let dropDownTableView = UITableView()
+  
+  var selectedButton = UIButton()
+  
+  func addTransparentView(frames: CGRect) {
+    let window = UIApplication.shared.windows.first {$0.isKeyWindow}
+    transparentView.frame = window?.frame ?? self.view.frame
+    view.addSubview(transparentView)
+    
+    dropDownTableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+    view.addSubview(dropDownTableView)
+    dropDownTableView.layer.cornerRadius = 5
+    
+    transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+    dropDownTableView.reloadData()
+    
+    let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+    transparentView.addGestureRecognizer(tapgesture)
+    transparentView.alpha = 0
+    UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations:{
+      self.transparentView.alpha = 0.5
+      self.dropDownTableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: CGFloat(self.dropdownDataSource.dataList.count * 50))
+    }, completion: nil)
+
+  }
+  
+  @objc func removeTransparentView() {
+    let frames = selectedButton.frame
+    UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations:{
+      self.transparentView.alpha = 0.0
+      self.dropDownTableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y, width: frames.width, height: 0)
+    }, completion: nil)
+  }
+  
+  @IBAction func didTapSetOrderButton(_ sender: UIButton) {
+    selectedButton = setOrderButton
+    addTransparentView(frames: setOrderButton.frame)
   }
 }
 
 extension SearchResultViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return "근처 장소를 소개해드릴게요"
-  }
-  
-  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 80))
-    header.addBorder([.top], color: .색e8, width: 1.0)
-    let headerLabel = UILabel()
-    headerLabel.frame = CGRect(x: 16, y: 37, width: header.frame.width-16, height: 21)
-    headerLabel.text = "근처 장소를 소개해드릴게요"
-    headerLabel.font = .robotoBold(size: 18)
-    headerLabel.textColor = UIColor.색44444
-    header.backgroundColor = .white
-    header.addSubview(headerLabel)
-    return header
-  }
-  
-  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-    return 80
-  }
-  
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return tableView.frame.width / 343 * 245
+    if tableView == resultTableView {
+      return tableView.frame.width / 343 * 245
+    } else {
+      return CGFloat(50)
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if tableView == dropDownTableView {
+      guard var oldValue = selectedButton.currentTitle else {
+        return
+      }
+      oldValue = String(oldValue.dropLast(2))
+      selectedButton.setTitle("\(dropdownDataSource.dataList[indexPath.row]) ▼", for: .normal)
+      dropdownDataSource.dataList.remove(at: indexPath.row)
+      dropdownDataSource.dataList.append(oldValue)
+      removeTransparentView()
+    }
   }
 }
+
+
