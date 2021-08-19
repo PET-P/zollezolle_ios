2d4d38ba19ce7c7b713389dd4d16c3a0d18c5c80
DOHYUN CHUNG <pastapeter@naver.com>
2021-08-20 05:17:25 +0900

    feat: 숙소, 카페, 관광지, 음식점에 대한 데이터소스 완성

---
+++ jjollaejjollae/jjollaejjollae/Screen/Search/View/LandmarkDataSource.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDataSource.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/08/19.
+//
+
+import UIKit
+
+class LandmarkDataSource: NSObject, UITableViewDataSource {
+  var dataList: [SearchResultInfo] = []
+  lazy var likes: [Int: Int] = [:]
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //return landmarkDataList.count
+    return 10
+  }
+}
+
+extension LandmarkDataSource: SearchResultCellDelegate {
+  
+  func didTapHeart(for index: Int, like: Bool) {
+    if like {
+      likes[index] = 1
+    } else {
+      likes[index] = 0
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath) as? SearchResultTableViewCell
+    else { return UITableViewCell() }
+    
+    cell.delegate = self
+    cell.index = indexPath.row
+    
+    let item = dataList[indexPath.row]
+    cell.locationNameLabel.text = item.name
+    cell.locationTypeLabel.text = item.type
+    cell.numberOfReviewsLabel.text = "(\(item.numbers))"
+    cell.starPointLabel.text = "\(item.points)"
+    
+    cell.isWish = likes[indexPath.row] == 1 ? true : false
+    dataList[indexPath.row].like = likes[indexPath.row] == 1 ? true : false
+    
+    
+    return cell
+  }
+  
+}
