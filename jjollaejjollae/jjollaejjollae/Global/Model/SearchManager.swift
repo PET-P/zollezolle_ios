830589d8d75fbc5f9b1c1c2f5876c647326643d7
DOHYUN CHUNG <pastapeter@naver.com>
2021-08-23 03:14:50 +0900

    test: test 할 수 있도록 임시 모델 변경

--- jjollaejjollae/jjollaejjollae/Global/Model/SearchManager.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/SearchManager.swift
@@ -67,16 +67,19 @@ extension SearchManager {
 }
 
 struct SearchResultInfo {
-  var id = 1 //여기는 다다를예정
-  var name = "아지멍카페"
-  var type = "커피/브런치"
-  var points = 4.9
-  var numbers = 52
-  var like = false
+  var id: Int = 1 //여기는 다다를예정
+  var name: String = "아지멍카페"
+  var location: String?
+  var type: String?
+  var points: Double? = 4.9
+  var numbers: Int? = 52
+  var like: Bool = false
+  var days: Int?
+  var prices: Int?
 }
 
 class ModelController: NSObject {
-  var accommoList = [SearchResultInfo(id: 0, name: "여기는 숙소"), SearchResultInfo(id: 1), SearchResultInfo(id: 2), SearchResultInfo(id: 3), SearchResultInfo(id: 666), SearchResultInfo(id: 333), SearchResultInfo(id: 99), SearchResultInfo(id: 12), SearchResultInfo(id: 37), SearchResultInfo(id: 1123123)]
+  var accommoList = [SearchResultInfo(id: 0, name: "애월코지하우스", location: "제주 애월읍", type: nil, points: 4.7, numbers: 102, days: 1, prices: 85000), SearchResultInfo(id: 1, name: "아지멍카페", location: nil, type: "커피/브런치", points: 4.9, numbers: 52, days: nil, prices: nil), SearchResultInfo(id: 2, name: "애월코지하우스", location: "제주 애월읍", type: nil, points: 4.7, numbers: 102, days: 1, prices: 85000), SearchResultInfo(id: 3, name: "애월코지하우스", location: "제주 애월읍", type: nil, points: 4.7, numbers: 102, days: 1, prices: 85000), SearchResultInfo(id: 666, name: "아지멍카페", location: nil, type: "커피/브런치", points: 4.9, numbers: 52, days: nil, prices: nil), SearchResultInfo(id: 333, name: "아지멍카페", location: nil, type: "커피/브런치", points: 4.9, numbers: 52, days: nil, prices: nil), SearchResultInfo(id: 99), SearchResultInfo(id: 12, name: "애월코지하우스", location: "제주 애월읍", type: nil, points: 4.7, numbers: 102, days: 1, prices: 85000), SearchResultInfo(id: 37, name: "아지멍카페", location: nil, type: "커피/브런치", points: 4.9, numbers: 52, days: nil, prices: nil), SearchResultInfo(id: 1123123, name: "아지멍카페", location: nil, type: "커피/브런치", points: 4.9, numbers: 52, days: nil, prices: nil)]
   var cafeList = [SearchResultInfo(id: 0, name: "여기는 카페"), SearchResultInfo(id: 1), SearchResultInfo(id: 2), SearchResultInfo(id: 3), SearchResultInfo(id: 666), SearchResultInfo(id: 333), SearchResultInfo(id: 99), SearchResultInfo(id: 12), SearchResultInfo(id: 37), SearchResultInfo(id: 1123123)]
   var landmarkList = [SearchResultInfo(id: 0, name: "여기는 관광지"), SearchResultInfo(id: 1), SearchResultInfo(id: 2), SearchResultInfo(id: 3), SearchResultInfo(id: 666), SearchResultInfo(id: 333), SearchResultInfo(id: 99), SearchResultInfo(id: 12), SearchResultInfo(id: 37), SearchResultInfo(id: 1123123)]
   var restaurantList = [SearchResultInfo(id: 0, name: "여기는 맛집"), SearchResultInfo(id: 1), SearchResultInfo(id: 2), SearchResultInfo(id: 3), SearchResultInfo(id: 666), SearchResultInfo(id: 333), SearchResultInfo(id: 99), SearchResultInfo(id: 12), SearchResultInfo(id: 37), SearchResultInfo(id: 1123123)]
