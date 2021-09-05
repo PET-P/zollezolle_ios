01dc19e1baca30ae6dc0973f742b46b8296507f1
Woozzang <woozzaang@gmail.com>
2021-09-05 22:16:00 +0900

    feat: Line Spacing 기능 추가

--- jjollaejjollae/jjollaejjollae/Global/Extension/UILabel+Extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UILabel+Extension.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 extension UILabel {
-    func titleStyle(text: String) {
-        self.text = "\(text)"
-        self.font = UIFont.robotoBold(size: 30)
-        self.textColor = UIColor.쥐색38
+  
+  func titleStyle(text: String) {
+      self.text = "\(text)"
+      self.font = UIFont.robotoBold(size: 30)
+      self.textColor = UIColor.쥐색38
+  }
+  
+  func setLineSpacing(spacing: CGFloat) {
+    
+    if let text = self.text {
+      let attributedString = NSMutableAttributedString(string: text)
+      let style = NSMutableParagraphStyle()
+      
+      style.lineSpacing = spacing
+      attributedString.addAttributes([.paragraphStyle: style], range: NSRange(location: 0, length: attributedString.length))
+      
+      self.attributedText = attributedString
+      
+      print(self.attributedText)
     }
+  }
 }
