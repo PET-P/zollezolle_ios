2ff065b7e7070a7727da692ac94692e502734e4c
DOHYUN CHUNG <pastapeter@naver.com>
2021-09-15 08:03:15 +0900

    feat: DogInfoManager로 강아지 정보 UserDefaults 관리

--- jjollaejjollae/jjollaejjollae/Global/Model/DogInfoManager.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/DogInfoManager.swift
@@ -7,52 +7,28 @@
 
 import Foundation
 
-struct DogInfo {
-  var name: String?
-  var age: Int?
-  var gender: Gender
-  var size: Size
-  var weight: Int?
-  var type: String?
-  var imageURL: String?
-}
-
-enum Gender: Int {
-  case male = 0
-  case female = 1
-}
-
-enum Size: Int {
-  case small = 0
-  case middle = 1
-  case big = 2
-}
-
 class DogInfoManager {
   
   private let defaults = UserDefaults.standard
   private let key = "dogInfo"
-  private var dogInfos: [DogInfo]?
-  
-  func saveDogInfo(with dogInfos: [DogInfo]) {
-    defaults.set(dogInfos, forKey: key)
-    self.dogInfos = dogInfos
-  }
-  
-  func getDogInfo() -> [DogInfo] {
-    return getFromUserDefaults(for: key)
-  }
-  
-  func updateDogInfo(at index: Int, with dogInfo: DogInfo) {
-    if var dogInfos = self.dogInfos {
-      dogInfos[index] = dogInfo
-      defaults.set(dogInfos, forKey: key)
-    } else {
-      return
+    
+  var dogInfos: [DogInfo] {
+    get {
+      return defaults.array(forKey: key) as? [DogInfo] ?? []
+    }
+    set(new) {
+      defaults.set(new, forKey: key)
+      defaults.synchronize()
     }
   }
   
-  private func getFromUserDefaults(for key: String) -> [DogInfo] {
-    return defaults.array(forKey: key) as? [DogInfo] ?? []
+  func updateDogInfo(at index: Int, with dogInfo: DogInfo) {
+    var tempDogInfos = dogInfos
+    tempDogInfos[index] = dogInfo
+    dogInfos = tempDogInfos
+  }
+  
+  var isDogInfosEmpty: Bool {
+    return dogInfos.isEmpty
   }
 }
