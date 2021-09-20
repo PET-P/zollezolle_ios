1b66b3336f69de005b85bd42a00126229d3f8024
pastapeter <pastapeter@naver.com>
2021-09-20 23:54:59 +0900

    feat: 회원가입 시에 받는 데이터와, 회원가입시 넘겨줘야하는 데이터 형 생성

---
+++ jjollaejjollae/jjollaejjollae/Global/Model/SignUpData.swift
@@ -0,0 +1,28 @@
+//
+//  SignUp.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/09/20.
+//
+
+import Foundation
+
+struct SignUpData: Codable {
+  let id: String?
+  let accessToken: String?
+  let refreshToken: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+    case accessToken = "accessToken"
+    case refreshToken = "refreshToKen"
+  }
+}
+
+
+struct SignUp {
+  var email: String = ""
+  var password: String = ""
+  var nick: String = ""
+  var phone: String = ""
+}
