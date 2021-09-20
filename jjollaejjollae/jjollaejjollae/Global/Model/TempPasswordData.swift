546487c3d742783948ae896240f5220401dd426e
pastapeter <pastapeter@naver.com>
2021-09-20 23:55:20 +0900

    feat: 임시비밀번호에 대한 response 생성

---
+++ jjollaejjollae/jjollaejjollae/Global/Model/TempPasswordData.swift
@@ -0,0 +1,14 @@
+//
+//  TempPassword.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/09/20.
+//
+
+import Foundation
+
+struct TempPasswordData: Codable {
+  let success: Bool?
+  let message: String?
+  let tempPassword: String?
+}
