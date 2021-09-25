c93df1cfd67d7e95c0c2c8d7c5989d0e79daae9e
pastapeter <pastapeter@naver.com>
2021-09-26 01:40:09 +0900

    feat: 로그인매니저에 keychain CRUD 설정

--- jjollaejjollae/jjollaejjollae/Global/Model/LoginManager.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/LoginManager.swift
@@ -6,8 +6,12 @@
 //
 
 import Foundation
+import Security
 
 struct LoginManager {
+  
+  private let defaults = UserDefaults.standard
+  private let bundleName = "P.TEP.jjollaejjollae"
   
   func isValidEmail(email: String) -> Bool {
     //이메일 유효성 확인 정규식
@@ -25,6 +29,63 @@ struct LoginManager {
   
   func isPasswordVerified(confirmation: String) -> Bool {
     return false
+  }
+  
+  func saveToken(accessToken: String, refreshToken: String) {
+    defaults.set(accessToken, forKey: "accessToken")
+    defaults.set(refreshToken, forKey: "refreshToken")
+  }
+  
+  func saveInKeychain(account: String, value: String) {
+    // 키체인쿼리
+    let keyChainQuery: NSDictionary = [
+      kSecClass: kSecClassGenericPassword, //아이탬 클레스
+      kSecAttrService: bundleName, //앱 번들 아이디
+      kSecAttrAccount: account, //사용자 계정
+      kSecValueData: value.data(using: .utf8, allowLossyConversion: false)! //저장할 값
+    ]
+    
+    // 값 삭제
+    SecItemDelete(keyChainQuery)
+    
+    // 새로운 키체인 아이템 등록
+    let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+    assert(status == noErr, "토큰 값 저장에 실패")
+    NSLog("status=\(status)")
+  }
+  
+  func loadFromKeychain(account: String) -> String? {
+    let keyChainQuery: NSDictionary = [
+      kSecClass: kSecClassGenericPassword, //아이탬 클레스
+      kSecAttrService: bundleName, //앱 번들 아이디
+      kSecAttrAccount: account, //사용자 계정
+      kSecReturnData: kCFBooleanTrue, // CFData으로 불러오라
+      kSecMatchLimit: kSecMatchLimitOne // 중복되는 경우 한개만
+    ]
+    
+    var dataTypeRef: AnyObject?
+    let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+    
+    if status == errSecSuccess {
+      let retrievedData = dataTypeRef as! Data
+      let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+      return value
+    } else {
+      print("토큰 로드 실패, status = \(status)")
+      return nil
+    }
+  }
+  
+  func deleteFromKeyChain(account: String) {
+    let keyChainQuery: NSDictionary = [
+      kSecClass: kSecClassGenericPassword, //아이탬 클레스
+      kSecAttrService: bundleName, //앱 번들 아이디
+      kSecAttrAccount: account, //사용자 계정
+    ]
+    
+    let status = SecItemDelete(keyChainQuery)
+    assert(status == noErr, "토큰 값 삭제에 실패했습니다")
+    NSLog("status=\(status)")
   }
   
 }
