67f38e58d2d999881456cc313601440635dab682
pastapeter <pastapeter@naver.com>
2021-10-11 04:23:36 +0900

    feat: PetData 추가

--- jjollaejjollae/jjollaejjollae/Global/Model/PetData.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/PetData.swift
@@ -7,16 +7,33 @@
 
 import Foundation
 
+// id는 필수적이고 
+
 struct PetData: Codable {
-  let id, name, sex, type, size: String
-  let age: Int?
-  let breed: String?
-  let weight: Double?
-  let imageUrl: String? //imageKey값
+  let id: String
+  var sex: Sex
+  var name, type: String
+  var size: Size
+  var age: Int?
+  var breed: String?
+  var weight: Double?
+  var imageUrl: String? //imageKey값
   
   enum CodingKeys: String, CodingKey {
     case age, size
     case id = "_id"
     case name, sex, type, breed, weight, imageUrl
   }
+  
+}
+
+enum Sex: String, Codable{
+  case female = "여"
+  case male = "남"
+}
+
+enum Size: String, Codable {
+  case small = "소형"
+  case middle = "중형"
+  case big = "대형"
 }
