9f8f12d43402885810dc4fca33b44e7ab9a1155d
pastapeter <pastapeter@naver.com>
2021-10-13 00:50:39 +0900

    feat: PetData 커스텀 init으로 직접 init 가능하게 변경

--- jjollaejjollae/jjollaejjollae/Global/Model/PetData.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/PetData.swift
@@ -18,13 +18,29 @@ struct PetData: Codable {
   var breed: String?
   var weight: Double?
   var imageUrl: String? //imageKey값
+  var isRepresent: Bool
   
   enum CodingKeys: String, CodingKey {
     case age, size
     case id = "_id"
-    case name, sex, type, breed, weight, imageUrl
+    case name, sex, type, breed, weight, imageUrl, isRepresent
   }
-  
+}
+
+extension PetData {
+  init(from decoder: Decoder) throws {
+    let value = try decoder.container(keyedBy: CodingKeys.self)
+    id = (try? value.decode(String.self, forKey: .id)) ?? "1"
+    sex = (try? value.decode(Sex.self, forKey: .sex)) ?? Sex.male
+    size = (try? value.decode(Size.self, forKey: .size)) ?? Size.small
+    name = (try? value.decode(String.self, forKey: .name)) ?? "쫄래쫄래1"
+    type = (try? value.decode(String.self, forKey: .type)) ?? "강아지"
+    age = try? value.decode(Int.self, forKey: .age)
+    breed = try? value.decode(String.self, forKey: .breed)
+    weight = try? value.decode(Double.self, forKey: .weight)
+    imageUrl = try? value.decode(String.self, forKey: .imageUrl)
+    isRepresent = (try? value.decode(Bool.self, forKey: .isRepresent)) ?? false
+  }
 }
 
 enum Sex: String, Codable{
