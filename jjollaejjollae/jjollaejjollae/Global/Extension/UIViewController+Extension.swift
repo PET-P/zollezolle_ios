3897c6501b64778a7470e372c440c9e791a07616
pastapeter <pastapeter@naver.com>
2021-10-17 21:25:24 +0900

    feat: UIViewController+ Extension에서 메모리 릭 찾는 코드 추가

---
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UIViewController+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Extensino.swift
+//  jjollaejjollae
+//
+//  Created by abc on 2021/10/16.
+//
+
+import UIKit
+
+extension UIViewController {
+  func didReceiveMemoryWarning() {
+    print("메모리 부족은 이 함수로부터 실행된다😅😅 \(#function)")
+  }
+}
