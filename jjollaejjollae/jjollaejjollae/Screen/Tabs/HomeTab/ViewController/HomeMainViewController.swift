72d341da3bf9babebb91da3728e185ba9ab64afc
Woozzang <woozzaang@gmail.com>
2021-10-18 20:06:03 +0900

    Feat: 여행지 셀 사진 넣기 #35

--- jjollaejjollae/jjollaejjollae/Screen/Tabs/HomeTab/ViewController/HomeMainViewController.swift
+++ jjollaejjollae/jjollaejjollae/Screen/Tabs/HomeTab/ViewController/HomeMainViewController.swift
@@ -26,6 +26,7 @@ class HomeMainViewController: UIViewController, StoryboardInstantiable {
       if hasMainPhoto {
         // TODO: 프로필 사진 설정
         
+        
         return
       }
       
@@ -50,7 +51,12 @@ class HomeMainViewController: UIViewController, StoryboardInstantiable {
     }
   }
   
-  @IBOutlet weak var firstCollectionView: UICollectionView!
+  @IBOutlet weak var locationCollectionView: UICollectionView! {
+    didSet {
+      locationCollectionView.showsHorizontalScrollIndicator = false
+    }
+  }
+  
   @IBOutlet weak var secondCollectionView: UICollectionView!
   @IBOutlet weak var thirdCollectionView: UICollectionView!
   
@@ -74,9 +80,10 @@ class HomeMainViewController: UIViewController, StoryboardInstantiable {
     Author : 박우찬
     NibName 과 reuseIdentifier 를 동일하게 설정
    */
-  private let homeLocationCellNibName = "HomeLocationCell"
+//  private let homeLocationCellNibName = "HomeLocationCell"
   
   private let homeTipCellNibName = "HomeTipCell"
+  
 
   // MARK: - Life Cycle
   override func viewDidLoad() {
@@ -85,82 +92,148 @@ class HomeMainViewController: UIViewController, StoryboardInstantiable {
     
     mainScrollView.delegate = self
     
-    let layout = firstCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+    
+
+    setUpLocationCollectionView()
+    
+//    secondCollectionView.delegate = self
+//    secondCollectionView.dataSource = self
+////
+//    let tipCellNib = UINib(nibName: homeTipCellNibName, bundle: nil)
+//    // TODO: HomeTipCell 로 변경
+//    secondCollectionView.register(tipCellNib, forCellWithReuseIdentifier: homeTipCellNibName)
+
+//    thirdCollectionView.delegate = self
+//    thirdCollectionView.dataSource = self
+//
+//    thirdCollectionView.register(locationCellNib, forCellWithReuseIdentifier: homeLocationCellNibName)
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    
+  }
+  
+  // MARK: - Custom
+  
+  /**
+    LocationCollectionView 기본 셋업 메서드
+   */
+  func setUpLocationCollectionView() {
+    
+    let layout = locationCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
     
     layout.itemSize = CGSize(width: 138, height: 72)
     layout.minimumLineSpacing = 15
     layout.scrollDirection = .horizontal
     
-    firstCollectionView.delegate = self
-    firstCollectionView.dataSource = self
+    locationCollectionView.delegate = self
+    locationCollectionView.dataSource = self
     
-    let locationCellNib = UINib(nibName: homeLocationCellNibName, bundle: nil)
-  
-    firstCollectionView.register(locationCellNib, forCellWithReuseIdentifier: homeLocationCellNibName)
-
-    secondCollectionView.delegate = self
-    secondCollectionView.dataSource = self
-//
-    let tipCellNib = UINib(nibName: homeTipCellNibName, bundle: nil)
-    // TODO: HomeTipCell 로 변경
-    secondCollectionView.register(tipCellNib, forCellWithReuseIdentifier: homeTipCellNibName)
-
-    thirdCollectionView.delegate = self
-    thirdCollectionView.dataSource = self
+    let locationCellNib = UINib(nibName: HomeLocationCollectionViewCell.identifier, bundle: nil)
     
-    thirdCollectionView.register(locationCellNib, forCellWithReuseIdentifier: homeLocationCellNibName)
-    
+    locationCollectionView.register(locationCellNib, forCellWithReuseIdentifier: HomeLocationCollectionViewCell.identifier)
   }
-  
-  // MARK: - Custom
 
 }
 
-extension HomeMainViewController: UIScrollViewDelegate {
-  
-  func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    
-    mainImageView.alpha = ((mainImageView.frame.height - mainImageMinHeight) / (mainImageMaxHeight - mainImageMinHeight))
-    
-    let value = mainImageMaxHeight - scrollView.contentOffset.y.rounded(.up) - CGFloat(356)
-    
-    if value >= mainImageMinHeight && value <= mainImageMaxHeight {
-      imageBackViewHeightConstraint.constant = value
-    }
-  }
-}
 
 extension HomeMainViewController: UICollectionViewDataSource {
+  
+  enum Location: CaseIterable {
+    case seoul
+    case sokcho
+    case gangneung
+    case yeosu
+    case gyeongju
+    case daegu
+    case jeju
+    
+    var description: String {
+      
+      switch self {
+        case .seoul:
+          return "서울"
+        case .sokcho:
+          return "속초"
+        case .gangneung:
+          return "강릉"
+        case .yeosu:
+          return "여수"
+        case .gyeongju:
+          return "경주"
+        case .daegu:
+          return "대구"
+        case .jeju:
+          return "제주"
+      }
+    }
+  }
 
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    if collectionView === locationCollectionView {
+      return Location.allCases.count
+    }
 
     return 10
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-    if collectionView === secondCollectionView {
-      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: homeTipCellNibName, for: indexPath) as? HomeTipCollectionViewCell else { fatalError(#function) }
+    
+    if collectionView === locationCollectionView {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeLocationCollectionViewCell.identifier, for: indexPath)
       
-      cell.heightAnchor.constraint(equalToConstant: 120).isActive = true
-      cell.widthAnchor.constraint(equalToConstant: 116).isActive = true
-
-      return cell
+      guard let locationCell = cell as? HomeLocationCollectionViewCell else { return UICollectionViewCell() }
+      locationCell.mainImageView.image = UIImage(named: Location.allCases[indexPath.item].description)
       
+      return locationCell
     } else {
-      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: homeLocationCellNibName, for: indexPath) as? HomeLocationCollectionViewCell else { fatalError(#function) }
-      
-      cell.heightAnchor.constraint(equalToConstant: 72).isActive = true
-      cell.widthAnchor.constraint(equalToConstant: 138).isActive = true
-
-      return cell
-      
+      return UICollectionViewCell()
     }
     
+//
+//
+//    if collectionView === secondCollectionView {
+//      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: homeTipCellNibName, for: indexPath) as? HomeTipCollectionViewCell else { fatalError(#function) }
+//
+//      cell.heightAnchor.constraint(equalToConstant: 120).isActive = true
+//      cell.widthAnchor.constraint(equalToConstant: 116).isActive = true
+//
+//      return cell
+//
+//    } else {
+//      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: homeLocationCellNibName, for: indexPath) as? HomeLocationCollectionViewCell else { fatalError(#function) }
+//
+//      cell.heightAnchor.constraint(equalToConstant: 72).isActive = true
+//      cell.widthAnchor.constraint(equalToConstant: 138).isActive = true
+//
+//      return cell
+//
+//    }
+//
   }
+  
+  
+  
 }
 
 extension HomeMainViewController: UICollectionViewDelegate {
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    if collectionView === locationCollectionView {
+      
+//      let resultVC = SearchNoResultViewControlle
+      
+//      self.navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+    }
+  }
+  
   
   /**
     CollectionView Cell Center Align
@@ -176,7 +249,24 @@ extension HomeMainViewController: UICollectionViewDelegate {
 //
 //      return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
 //  }
+  
+  
 }
 
 
+// MARK: - UIScrollViewDelegate
+
+extension HomeMainViewController: UIScrollViewDelegate {
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+    mainImageView.alpha = ((mainImageView.frame.height - mainImageMinHeight) / (mainImageMaxHeight - mainImageMinHeight))
+    
+    let value = mainImageMaxHeight - scrollView.contentOffset.y.rounded(.up) - CGFloat(356)
+    
+    if value >= mainImageMinHeight && value <= mainImageMaxHeight {
+      imageBackViewHeightConstraint.constant = value
+    }
+  }
+}
 
