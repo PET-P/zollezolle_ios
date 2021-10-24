cee20ae1a0b6df9884c0604c76e3eca3c75a7195
pastapeter <pastapeter@naver.com>
2021-10-25 00:56:54 +0900

    chores: Wishlist folder data -> wishlistdata로 변경

--- jjollaejjollae/jjollaejjollae/Global/Model/WishlistData.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/WishlistData.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 struct WishlistData: Codable {
+  var total: [String]
+  var totalCount: Int
+  var folderCount: Int
+  var id, userID: String
+  var folder: [FolderData]
+  let createdAt, updatedAt: String
+  let v: Int
   
+  enum CodingKeys: String, CodingKey {
+    case total, totalCount, folderCount
+    case id = "_id"
+    case userID = "userId"
+    case folder, createdAt, updatedAt
+    case v = "__v"
+  }
 }
+
+
+
+
