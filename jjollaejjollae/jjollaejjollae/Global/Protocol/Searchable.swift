c3a0f9490d741069514526a5f4f69a30ef59ebc4
pastapeter <pastapeter@naver.com>
2021-10-26 00:27:21 +0900

    fix: Searchable에서 검색결과없을때 새로운 APi 통신 기능을 VC의 willappear로 옮김

--- jjollaejjollae/jjollaejjollae/Global/Protocol/Searchable.swift
+++ jjollaejjollae/jjollaejjollae/Global/Protocol/Searchable.swift
@@ -14,58 +14,40 @@ protocol SearchDataReceiveable: NSObject {
 
 extension SearchDataReceiveable {
   func gotoSearchVC(from caller: UIViewController) {
-    guard let searchVC = SearchViewController.loadFromStoryboard()
-            as? SearchViewController else {return}
-    caller.navigationController?.popToViewController(searchVC, animated: true)
+    caller.navigationController?.popViewController(animated: true)
   }
 }
 
 protocol Searchable: NSObject {
-  func sendRightVC(by text: String, with result: [SearchResultData]) -> UIViewController
+  func sendRightVC(from viewController: UIViewController, by text: String, with result: [SearchResultData]) -> UIViewController
 }
 
 extension Searchable {
   
-  func sendRightVC(by text: String, with result: [SearchResultData]) -> UIViewController {
+  func sendRightVC(from viewController: UIViewController, by text: String, with result: [SearchResultData]) -> UIViewController {
     let afterTrimText = text.trimmingCharacters(in: .whitespacesAndNewlines)
-    SearchManager.shared.searchText = afterTrimText
     if result.count == 0 {
       //TODO: 위치모드를 나눠야할 것 같다.
-      var noResultVC: SearchNoResultViewController?
-      APIService.shared.nearPlace(latitude: "127.12", longitude: "125.12") { (result) in
-        switch result {
-        case .success(let data):
-          noResultVC = SearchNoResultViewController.loadFromStoryboard() as? SearchNoResultViewController
-          guard let noResultVC = noResultVC else {
-            return
-          }
-          noResultVC.newDataList = data
-        case .failure(let error):
-          print("error: \(error)")
-          noResultVC = SearchNoResultViewController.loadFromStoryboard() as? SearchNoResultViewController
-          guard let noResultVC = noResultVC else {
-            return
-          }
-          noResultVC.newDataList = []
-        }
+      guard let noSearchVC = SearchNoResultViewController.loadFromStoryboard() as? SearchNoResultViewController else {return SearchNoResultViewController()}
+      return noSearchVC
+    } else {
+      switch afterTrimText {
+      case LocationName.jeju.description, LocationName.gyeongju.description,
+        LocationName.daegu.description, LocationName.gangneung.description,
+        LocationName.seoul.description, LocationName.sokcho.description,
+        LocationName.yeosu.description:
+        guard let searchLocationVC = SearchWithLocationViewController.loadFromStoryboard()
+                as? SearchWithLocationViewController else {return SearchWithLocationViewController()}
+        return searchLocationVC
+      default:
+        guard let searchResultVC = SearchResultViewController.loadFromStoryboard()
+                as? SearchResultViewController else {return SearchResultViewController()}
+        searchResultVC.setMode(from: viewController)
+        return searchResultVC
       }
-      guard let noResultVC = noResultVC else {
-        return SearchNoResultViewController()
-      }
-      return noResultVC
-    }
-    switch afterTrimText {
-    case LocationName.jeju.description, LocationName.gyeongju.description,
-      LocationName.daegu.description, LocationName.gangneung.description,
-      LocationName.seoul.description, LocationName.sokcho.description,
-      LocationName.yeosu.description:
-      guard let searchLocationVC = SearchWithLocationViewController.loadFromStoryboard()
-              as? SearchWithLocationViewController else {return SearchWithLocationViewController()}
-      return searchLocationVC
-    default:
-      guard let searchResultVC = SearchResultViewController.loadFromStoryboard()
-              as? SearchResultViewController else {return SearchResultViewController()}
-      return searchResultVC
     }
   }
 }
+
+
+
