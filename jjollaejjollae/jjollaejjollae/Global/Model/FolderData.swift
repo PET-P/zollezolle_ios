a6e80999676f5632cb5d6d3bc1292a79b503bca1
pastapeter <pastapeter@naver.com>
2021-10-28 19:46:33 +0900

    feat: folderData 커스텀 디코딩

--- jjollaejjollae/jjollaejjollae/Global/Model/FolderData.swift
+++ jjollaejjollae/jjollaejjollae/Global/Model/FolderData.swift
@@ -10,14 +10,29 @@ import Foundation
 struct FolderData: Codable {
   var regions: [String]
   var count: Int
-  var contents: [SearchResultData]
   var id, name: String
+  var contents: [SearchResultData]?
   var startDate, endDate, updatedAt, createdAt: String?
   
   enum CodingKeys: String, CodingKey {
-    case regions, count, contents
+    case regions, count
     case id = "_id"
-    case name, startDate, endDate, updatedAt, createdAt
+    case name, startDate, endDate, updatedAt, createdAt, contents
+  }
+}
+extension FolderData {
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = (try? container.decode(String.self, forKey: .id)) ?? "1"
+    name = (try? container.decode(String.self, forKey: .name)) ?? "나만의 위시리스트"
+    count =  (try? container.decode(Int.self, forKey: .count)) ?? 0
+    regions = (try? container.decode([String].self, forKey: .regions)) ?? []
+    contents = (try? container.decode([SearchResultData].self, forKey: .contents)) ?? []
+    startDate = (try? container.decode(String.self, forKey: .startDate)) ?? ""
+    endDate = (try? container.decode(String.self, forKey: .endDate)) ?? ""
+    updatedAt = (try? container.decode(String.self, forKey: .updatedAt)) ?? ""
+    createdAt = (try? container.decode(String.self, forKey: .createdAt)) ?? ""
+    
   }
 }
 
