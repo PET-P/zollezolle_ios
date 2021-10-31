187147bc3bbd314eea2e0bc4ff7845cfbb45bd1d
pastapeter <pastapeter@naver.com>
2021-11-01 01:37:08 +0900

    bug: 캐싱키값변경으로  캐싱이 안되는것 해결

--- jjollaejjollae/jjollaejjollae/Global/Extension/UIImageView+Extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UIImageView+Extension.swift
@@ -14,10 +14,12 @@ extension UIImageView {
     let cache = ImageCache.default
     guard let newUrlString = UrlString.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {return}
     let imageUrl  = "\(StorageService.shared.imageBaseUrl)\(newUrlString)"
-    cache.retrieveImage(forKey: newUrlString) { (result) in
+    print("캐싱한것 찾을떄 쓰이는 url \(imageUrl)")
+    cache.retrieveImage(forKey: imageUrl) { (result) in
       switch result {
       case .success(let value):
         if let image = value.image {
+          print("캐시가 된것을 꺼내쓴다.")
           self.image = image
         } else {
           let storage = Storage.storage()
@@ -29,8 +31,9 @@ extension UIImageView {
             guard let url = url else {
               return
             }
-            print("url???", url)
-            self.kf.setImage(with: url)
+            print("이미지 캐시가 되지 않아서 다시 다운받는다.", url)
+            let resource = ImageResource(downloadURL: url, cacheKey: imageUrl)
+            self.kf.setImage(with: resource)
           }
         }
       case .failure(let error):
@@ -39,3 +42,5 @@ extension UIImageView {
     }
   }
 }
+
+
