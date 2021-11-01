843c9e40792b83096fa822a2b6eec861f517ff9a
Woozzang <woozzaang@gmail.com>
2021-11-01 19:37:53 +0900

    Build : API Client 변경 , 앱 아이콘 추가

--- jjollaejjollae/jjollaejjollae/Screen/Splash/ViewController/LaunchScreenViewController.swift
+++ jjollaejjollae/jjollaejjollae/Screen/Splash/ViewController/LaunchScreenViewController.swift
@@ -9,21 +9,8 @@ import UIKit
 
 class LaunchScreenViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
