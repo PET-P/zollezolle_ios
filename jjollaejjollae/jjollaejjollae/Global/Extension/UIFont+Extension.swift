66fe87f387c7ef4a2c6f37a531de6173d2127801
Woozzang <woozzaang@gmail.com>
2021-11-06 01:06:31 +0900

    Feat: UIFont+Extension 에 Spoqa Han Sans Neo 추가

--- jjollaejjollae/jjollaejjollae/Global/Extension/UIFont+Extension.swift
+++ jjollaejjollae/jjollaejjollae/Global/Extension/UIFont+Extension.swift
@@ -20,9 +20,47 @@ struct AppFontName {
     static let thinItalic = "RobotoMonoforPowerline-ThinItalic"
 }
 
+/*
+Family: Spoqa Han Sans Neo Font names: ["SpoqaHanSansNeo-Thin", "SpoqaHanSansNeo-Regular", "SpoqaHanSansNeo-Light", "SpoqaHanSansNeo-Medium", "SpoqaHanSansNeo-Bold"]
+*/
+
+fileprivate struct HanSansNeoFont {
+  
+  static let bold = "SpoqaHanSansNeo-Bold"
+  static let regular = "SpoqaHanSansNeo-Regular"
+  static let thin = "SpoqaHanSansNeo-Thin"
+  static let light = "SpoqaHanSansNeo-Light"
+  static let medium = "SpoqaHanSansNeo-Medium"
+}
+
 
 
 extension UIFont {
+  
+  /*
+   Spoqa Han Sans Neo
+   */
+  
+  static func hanSansNeoBold(size: CGFloat) -> UIFont {
+    return UIFont(name:HanSansNeoFont.bold, size: size)!
+  }
+  
+  static func hanSansNeoRegular(size: CGFloat) -> UIFont {
+    return UIFont(name:HanSansNeoFont.regular, size: size)!
+  }
+  
+  static func hanSansNeoThin(size: CGFloat) -> UIFont {
+    return UIFont(name:HanSansNeoFont.thin, size: size)!
+  }
+  
+  static func hanSansNeoLight(size: CGFloat) -> UIFont {
+    return UIFont(name:HanSansNeoFont.light, size: size)!
+  }
+  
+  static func hanSansNeoMedium(size: CGFloat) -> UIFont {
+    return UIFont(name:HanSansNeoFont.medium, size: size)!
+  }
+
     //MARK: - Roboto
     class func robotoBold(size: CGFloat) -> UIFont {
         return UIFont(name: AppFontName.bold, size: size)!
